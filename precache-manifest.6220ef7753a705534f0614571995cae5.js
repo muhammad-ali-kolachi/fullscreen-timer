b493895590282f6a09cdd4b457cc354531b3e1cd
Muhammad Ali Kolachi <factorde7@gmail.com>
2025-05-11 15:44:13 +0300

    Updates

---
+++ precache-manifest.6220ef7753a705534f0614571995cae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6631920a9c8dd5aecceb",
+    "url": "/fullscreen-timer/static/css/main.fe90b788.chunk.css"
+  },
+  {
+    "revision": "6631920a9c8dd5aecceb",
+    "url": "/fullscreen-timer/static/js/main.6631920a.chunk.js"
+  },
+  {
+    "revision": "eabb17bc67a77cf32e74",
+    "url": "/fullscreen-timer/static/js/runtime~main.eabb17bc.js"
+  },
+  {
+    "revision": "8a8cd1dc674c7720e55f",
+    "url": "/fullscreen-timer/static/js/2.8a8cd1dc.chunk.js"
+  },
+  {
+    "revision": "84724a4081294e3d7b714fcd85e14691",
+    "url": "/fullscreen-timer/index.html"
+  }
+];
